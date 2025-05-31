5907dba2df26ddce7497522264860f2cd6cf706f
miniharu <134501746+miniharu22@users.noreply.github.com>
2025-06-01 01:38:53 +0900

    UART Tx version 2 with Right shift

--- Testbench/UART/tb_uart.v
+++ Testbench/UART/tb_uart.v
@@ -1,45 +1,23 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/05/31 22:10:41
-// Design Name: 
-// Module Name: tb_uart
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-`timescale 1ns / 1ps
-
-
-module tb_uart ();
+module tb_uart();
 
     reg clk;
     reg reset;
-    reg start;
+    reg tx_start;
     reg [7:0] tx_data;
 
-    wire txd;
+    wire tx;
     wire tx_done;
 
     uart dut (
         .clk(clk),
         .reset(reset),
-        .start(start),
+        .tx_start(tx_start),
         .tx_data(tx_data),
 
-        .txd(txd),
+        .tx(tx),
         .tx_done(tx_done)
     );
 
@@ -49,14 +27,13 @@ module tb_uart ();
     initial begin
         clk = 1'b0;
         reset = 1'b1;
-        start = 1'b0;
+        tx_start = 1'b0;
         tx_data = 0;
     end
 
     initial begin
-        #20 reset = 1'b0;
-        #20 tx_data = 8'haf; start = 1'b1;
-        #10 start = 1'b0;
+        #100 reset = 1'b0;
+        #100 tx_data = 8'b11001010; tx_start = 1'b1;
+        #10 tx_start = 1'b0;
     end
-
 endmodule
